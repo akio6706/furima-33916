dbf65a4a2aec1f6038dd3bfb72c20c01cf48d8b7
akio6706 <campin616@gamil.com>
2021-02-10 22:21:00 +0900

    商品の出品ページ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,14 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :state_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :state_id
+    validates :burden_id
+    validates :area_id
+    validates :user
+  end
 
   belongs_to :user
   has_one_attached :image
