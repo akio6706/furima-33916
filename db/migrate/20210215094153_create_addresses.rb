3b2ec8ab898877e9cc2aec6f98684822d3b7ca4d
akio6706 <campin616@gamil.com>
2021-02-17 16:42:25 +0900

    商品購入機能の実装(修正2)

--- db/migrate/20210215094153_create_addresses.rb
+++ db/migrate/20210215094153_create_addresses.rb
@@ -5,8 +5,8 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer    :area_id,     null: false
       t.string     :city,        null: false
       t.string     :street,      null: false
-      t.string     :address,     null: false
-      t.string     :phone_number
+      t.string     :address
+      t.string     :phone_number,null: false
       t.references :order,       foreign_key: true
       t.timestamps
     end
